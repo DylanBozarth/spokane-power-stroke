6b29e6ca5a85df05905f1a00c49ef71e0cd5bf3b
Dylan <dylanabozarth@gmail.com>
2020-08-06 16:45:25 -0700

    working gallery

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import Mainpage from './components/mainpage';
-import { Services } from './components/services';
 import {Nav } from './components/navbar';
 import { Header } from './components/header';
+import ReactDOM from "react-dom";
+import { AnimatePresence } from "framer-motion";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Gallery } from "./components/Gallery";
+import { SingleImage } from "./components/SingleImage";
 
 function App() {
  
@@ -13,7 +17,18 @@ function App() {
     
       <Nav />
       <Header />
-     <Mainpage />
+      <Router>
+    <Route
+      render={({ location }) => (
+        <AnimatePresence exitBeforeEnter initial={false}>
+          <Switch location={location} key={location.pathname}>
+            <Route exact path="/" component={Gallery} />
+            <Route exact path="/image/:id" component={SingleImage} />
+          </Switch>
+        </AnimatePresence>
+      )}
+    />
+  </Router>
       
     </div>
   );
