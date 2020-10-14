536f3d3ecb2f5dcc12b490b7d8e837dc7985ec12
Dylan <dylanabozarth@gmail.com>
2020-10-14 15:17:18 -0700

    fixed font bug

--- src/components/pages/reviews.js
+++ src/components/pages/reviews.js
@@ -87,7 +87,7 @@ class Reviews extends Component {
   render() {
     return (
       <motion.div
-        className=""
+        className="reviewpage"
         initial="exit"
         animate="enter"
         exit="exit"
