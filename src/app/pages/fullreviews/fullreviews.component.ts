edb030da7dc30a14da824a955c28f4ec7d952e6a
dylan <dylanabozarth@gmail.com>
2021-11-05 12:35:17 -0700

    now routes

---
+++ src/app/pages/fullreviews/fullreviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fullreviews',
+  templateUrl: './fullreviews.component.html',
+  styleUrls: ['./fullreviews.component.css']
+})
+export class FullreviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
