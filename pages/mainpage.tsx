05b01738428493e569b89993df753e5244e1e8a3
dylan <dylanabozarth@gmail.com>
2021-11-06 19:25:54 -0700

    layout!

--- pages/mainpage.tsx
+++ pages/mainpage.tsx
@@ -1,12 +1,23 @@
+import { About } from "./components/about"
+import { Contact } from "./components/contact"
+import { Gallery } from "./components/gallery"
+import { Reviews } from "./components/reviews"
 import { Services } from "./components/services"
 
 export const MainPage = () => {
     return (
-        <div>MAINPAGE
+        <div>
 
-<h1>Having any of these problems? </h1>
+<h1 className="divider">Having any of these problems? </h1>
 <Services />
-
+<h1 className="divider">We have trained technicians ready to assist you</h1>
+<About />
+<h1 className="divider">We service all kinds of trucks</h1>
+<Gallery />
+<h1 className="divider">Hear from our satisfied customers</h1>
+<Reviews />
+<h1 className="divider">Stop by our shop or give us a call</h1>
+<Contact />
         </div>
     )
 }
