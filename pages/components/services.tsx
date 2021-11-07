dd55fb38e6f5e40a1abc2872d16b78b4a64809b4
dylan <dylanabozarth@gmail.com>
2021-11-06 19:15:00 -0700

    services layout maybe

---
+++ pages/components/services.tsx
@@ -0,0 +1,11 @@
+
+export const Services = () => {
+    return (
+<div className="componentContainer">
+
+<div className="item">a</div><div className="item">a</div><div className="item">a</div><div className="item">a</div><div className="item">a</div><div className="item">a</div>
+
+
+</div>
+    )
+}
--- pages/mainpage.tsx
+++ pages/mainpage.tsx
@@ -1,6 +1,12 @@
+import { Services } from "./components/services"
 
 export const MainPage = () => {
     return (
-        <div>MAINPAGE</div>
+        <div>MAINPAGE
+
+<h1>Having any of these problems? </h1>
+<Services />
+
+        </div>
     )
 }
