6141e0676dac46693ef41fab316fefea048c040a
dylan <dylanabozarth@gmail.com>
2021-11-11 09:08:09 -0800

    my own carosuel

--- pages/components/gallery.tsx
+++ pages/components/gallery.tsx
@@ -1,16 +1,15 @@
 import Image from 'next/image'
-import Carousel from 'react-elastic-carousel';
 import Engine from '../../public/images/gasengine.jpg'
 import brake from '../../public/images/brakenice.jpg'
 import rotor from '../../public/images/rotor.jpg'
 export const Gallery = () => {
     return(
         <div className="gallery">
-        <Carousel itemsToShow={3}>
+        <div className="gallerySlide">
             <Image src={Engine} alt="spokane-power-stroke" className="galleryImage" />
-            <Image src={brake} alt="power stroke" className="galleryImage" />
-            <Image src={rotor} alt="power stroke" className="galleryImage" />
-            </Carousel></div>
+            <Image src={brake}  alt="power stroke" className="galleryImage" />
+            <Image src={rotor}  alt="power stroke" className="galleryImage" /></div>
+           </div>
     )
 }
 // photos of repairs with little tooltips 
--- pages/mainpage.tsx
+++ pages/mainpage.tsx
@@ -1,23 +1,29 @@
-import { About } from "./components/about"
-import { Contact } from "./components/contact"
-import { Gallery } from "./components/gallery"
-import { Reviews } from "./components/reviews"
-import { Services } from "./components/services"
+import { About } from "./components/about";
+import { Contact } from "./components/contact";
+import { Gallery } from "./components/gallery";
+import { Reviews } from "./components/reviews";
+import { Services } from "./components/services";
 
 export const MainPage = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <h1 className="divider" id="services">Having any of these problems? </h1>
+      <Services />
 
-<h1 className="divider">Having any of these problems? </h1>
-<Services />
-<h1 className="divider">We have trained technicians ready to assist you</h1>
-<About />
-<h1 className="divider">We service all kinds of trucks</h1>
-<Gallery />
-<h1 className="divider">Hear from our satisfied customers</h1>
-<Reviews />
-<h1 className="divider">Stop by our shop or give us a call</h1>
-<Contact />
-        </div>
-    )
-}
+      <h1 className="divider" id="">
+        We service all kinds of trucks
+      </h1>
+      <Gallery />
+      <h1 className="divider" id="crew">
+        We have trained technicians ready to assist you
+      </h1>
+      <About />
+      <h1 className="divider" id="reviews">
+        Hear from our satisfied customers
+      </h1>
+      <Reviews />
+      <h1 className="divider" id="contact">Stop by our shop or give us a call</h1>
+      <Contact />
+    </div>
+  );
+};
