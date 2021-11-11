752dc28ca6e0638c9b7e6014c48fd182baef0400
dylan <dylanabozarth@gmail.com>
2021-11-10 20:54:18 -0800

    carosuel and error

--- pages/components/gallery.tsx
+++ pages/components/gallery.tsx
@@ -1,6 +1,16 @@
+import Image from 'next/image'
+import Carousel from 'react-elastic-carousel';
+import Engine from '../../public/images/gasengine.jpg'
+import brake from '../../public/images/brakenice.jpg'
+import rotor from '../../public/images/rotor.jpg'
 export const Gallery = () => {
     return(
-        <div>Gallery</div>
+        <div className="gallery">
+        <Carousel itemsToShow={3}>
+            <Image src={Engine} alt="spokane-power-stroke" className="galleryImage" />
+            <Image src={brake} alt="power stroke" className="galleryImage" />
+            <Image src={rotor} alt="power stroke" className="galleryImage" />
+            </Carousel></div>
     )
 }
 // photos of repairs with little tooltips 
