af762240bacae5d13b29752779f693e19841a04a
dylan <dylanabozarth@gmail.com>
2021-11-13 18:58:42 -0800

    grid

--- pages/components/gallery.tsx
+++ pages/components/gallery.tsx
@@ -1,15 +1,17 @@
-import Image from 'next/image'
-import Engine from '../../public/images/gasengine.jpg'
-import brake from '../../public/images/brakenice.jpg'
-import rotor from '../../public/images/rotor.jpg'
+import Image from "next/image";
+import { useState } from "react";
+import Engine from "../../public/images/gasengine.jpg";
+import brake from "../../public/images/brakenice.jpg";
+import rotor from "../../public/images/rotor.jpg";
 export const Gallery = () => {
-    return(
-        <div className="gallery">
-        <div className="gallerySlide">
-            <Image src={Engine} alt="spokane-power-stroke" className="galleryImage" />
-            <Image src={brake}  alt="power stroke" className="galleryImage" />
-            <Image src={rotor}  alt="power stroke" className="galleryImage" /></div>
-           </div>
-    )
-}
-// photos of repairs with little tooltips 
+  const [active, setActive] = useState(1);
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <Image src={Engine} alt="spokane-power-stroke" className="col-span-2" /> 
+      <Image src={brake} alt="power stroke" className="col-span-2" />
+      <Image src={rotor} alt="power stroke" className="col-span-2" />
+    </div>
+  );
+};
+// photos of repairs with little tooltips
+// gallery type slideshow 
--- pages/components/services.tsx
+++ pages/components/services.tsx
@@ -2,9 +2,7 @@
 export const Services = () => {
     return (
 <div className="text-xl font-medium text-black">
-
-<div className="text-xl font-medium text-black">a</div><div className="item">a</div><div className="item">a</div><div className="item">a</div><div className="item">a</div><div className="item">a</div>
-
+aaa
 
 </div>
     )
