517f3e9386f6eaed39af6ae8af73bd2062430aeb
dylan <dylanabozarth@gmail.com>
2021-11-17 12:42:58 -0800

    toggle services

--- pages/components/services.tsx
+++ pages/components/services.tsx
@@ -3,18 +3,24 @@ export const Services = () => {
     const [active, setActive] = useState(0)
     return (
 <div className="flex flex-wrap -mx-1 lg:-mx-4 text-center">
-      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-      Ford Powerstroke diesel repair</div>
-      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-      Gas engine repair</div>
-      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-      Brake Service</div>
-      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-      Steering system repair</div>
-      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-      Differentials</div>
-      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-      General Diesel Maintinence</div>
+      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" onClick={() => setActive(1)}>
+      Ford Powerstroke diesel repair
+      <p className={active  === 1 ? 'text' : 'hidden'}>Additional information</p></div>
+      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" onClick={() => setActive(2)}>
+      Gas engine repair
+      <p className={active  === 2 ? 'text' : 'hidden'}>Additional information</p></div>
+      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" onClick={() => setActive(3)}>
+      Brake Service
+      <p className={active  === 3 ? 'text' : 'hidden'}>Additional information</p></div>
+      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" onClick={() => setActive(4)}>
+      Steering system repair
+      <p className={active  === 4 ? 'text' : 'hidden'}>Additional information</p></div>
+      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" onClick={() => setActive(5)}>
+      Differentials
+      <p className={active  === 5 ? 'text' : 'hidden'}>Additional information</p></div>
+      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" onClick={() => setActive(6)}>
+      General Diesel Maintinence
+      <p className={active  === 6 ? 'text' : 'hidden'}>Additional information</p></div>
     </div>
     )
 }
