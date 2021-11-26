a4432db74f4d838ff04312c68c7d3b47833e0277
dylan <dylanabozarth@gmail.com>
2021-11-26 11:15:14 -0800

    logo and styling

--- pages/mainpage.tsx
+++ pages/mainpage.tsx
@@ -4,30 +4,34 @@ import { Services } from "./components/services";
 import { Reviews } from "./components/reviews";
 import { useState } from "react";
 import { Navbar } from "./components/navbar";
+import Image from "next/image";
+import Logo from '../public/assets/logotext.png'
 export const MainPage = () => {
   const [active, setActive] = useState(0);
   return (
     <div className="p-1 ">
       <Navbar active={active} setActive={setActive} />
+      
       <div className="py-10">
-        <h1 className="text-center px:5 text-xl" id="services">
+        <Image src={Logo} className="mx-auto"  alt="spokane power stroke" />
+        <h1 className="text-center px-1 text-xl  bg-blue-500 text-white " id="services">
           Having any of these problems?{" "}
         </h1>
        
 
-        <h1 className="text-center px-1 text-blue" id="">
+        <h1 className="text-center px-1  bg-blue-500 text-white" id="">
           We service all kinds of trucks
         </h1>
         <Services active={active} setActive={setActive} />
-        <h1 className="divider" id="crew">
+        <h1 className="text-center px-1  bg-blue-500 text-white" id="crew">
           We have trained technicians ready to assist you
         </h1>
         <About  active={active} setActive={setActive}  />
-        <h1 className="divider" id="reviews">
+        <h1 className="text-center px-1  bg-blue-500 text-white" id="reviews">
           Hear from our satisfied customers
         </h1>
         <Reviews  active={active} setActive={setActive}  />
-        <h1 className="divider" id="contact">
+        <h1 className="text-center px-1  bg-blue-500 text-white" id="contact">
           Stop by our shop or give us a call
         </h1>
         <Contact  active={active} setActive={setActive}  />
