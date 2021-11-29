30f533a3519c958f3fe17d3a1353ade5b0a2964b
dylan <dylanabozarth@gmail.com>
2021-11-28 20:53:53 -0800

    better iframe

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,10 @@ export const Contact = (props: any) => {
   return (
     <div className=" flex">
       
-      <div className=" py-6 ">
+      <div className="mapContainer">
       <iframe
         loading="lazy"
-        className=""
+        className="map"
         src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ98syR9EYnlQRSH85A1eUgVM&key=AIzaSyB9sySkqSx67a7l8pEhGxlxxLdP2hcnpWY"
       ></iframe> </div>
     </div>
