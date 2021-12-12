87266fec04b85870040f2e121ad156f7dd1ab9fc
imstupidpleasehelp <dylanabozarth@gmail.com>
2021-12-11 20:12:15 -0800

    image optimzation

--- components/about.tsx
+++ components/about.tsx
@@ -7,15 +7,15 @@ export const About = (props: any) => {
         <div className="flex flex-wrap -mx-1 lg:-mx-4 text-center" >
       <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
       Brandon Bozarth
-      <Image src={Brandon}  alt="mechanic" /><h3>Ford certified master Technician
+      <Image src={Brandon}  alt="mechanic" priority={true} /><h3>Ford certified master Technician
       {/* view cerfitication */}</h3></div>
       
       <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
       Alvin Finch
-      <Image src={Alvin}  alt="mechanic" /><h3 id="reviews">Ford certified master Technician</h3 ></div>
+      <Image src={Alvin}  alt="mechanic"  priority={true}  /><h3 id="reviews">Ford certified master Technician</h3 ></div>
       <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
       Dustin Andersen
-      <Image src={Dustin}  alt="mechanic" /><h3>Ford certified master Technician</h3 ></div>
+      <Image src={Dustin}  alt="mechanic"   priority={true} /><h3>Ford certified master Technician</h3 ></div>
     </div>
     )
 }
